d4c335a4c05a02a0d7fc6d828bc8f55d19346571
gunesh <gunesh26@gmail.com>
2019-11-27 19:05:56 +0530

    1 file modified, 1 added

--- reducers/index.js
+++ reducers/index.js
@@ -1,10 +1,12 @@
 import { combineReducers } from 'redux';
 import home from './home_reducer';
 import movie from './movie_reducer';
+import users from './user_reducer';
+
 
 const rootReducer = combineReducers({
   home,
-  movie
+  movie,
+  users
 })
-
 export default rootReducer;

0d716b228ca71250d23c0cca022dff5d51d1e623
gunesh <gunesh26@gmail.com>
2019-11-27 18:50:15 +0530

    1 file modified

--- config.js
+++ config.js
@@ -3,11 +3,13 @@ const API_KEY = '844dba0bfd8f3a4f3799f6130ef9e335';
 const IMAGE_BASE_URL ='https://image.tmdb.org/t/p/';
 const BACKDROP_SIZE = 'w1280'
 const POSTER_SIZE = 'w500'
+const USER_API_URL = 'https://jsonplaceholder.typicode.com/users'
 
 export {
   API_URL,
   API_KEY,
   IMAGE_BASE_URL,
   BACKDROP_SIZE,
-  POSTER_SIZE
+  POSTER_SIZE,
+  USER_API_URL
 }

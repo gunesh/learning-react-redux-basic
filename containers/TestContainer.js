adca30831fe7b259bc82eb02022c8644c02416c4
gunesh <gunesh26@gmail.com>
2019-11-27 19:41:11 +0530

    1 file modified

--- containers/TestContainer.js
+++ containers/TestContainer.js
@@ -1,32 +1,58 @@
-import React, { Component } from "react";
-import { connect } from "react-redux";
-import { getUsers, showLoadingSpinner } from "../actions";
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import {
+  getUsers,
+  showLoadingSpinner
+} from '../actions';
+
 
 class TestContainer extends Component {
-  componentDidMount() {
+  
+  componentDidMount(){
     this.getUsers();
   }
 
-  componentDidUpdate() {}
+  componentDidUpdate() {
+   
+  }
 
-  getUsers = () => {
+   getUsers = () => {
     this.props.showLoadingSpinner();
     this.props.getUsers();
-  };
+  }
+
+  getTemplate(element){
+    return (
+      <div>
+        <p>{element.title}</p>
+        <p>{element.backdrop_path}</p>
+        <p>{element.overview}</p>
+        <p>{element.release_date}</p>
+      </div>);
+  }
 
   render() {
-    return <>Test C{this.props.users.map((element, i) => "df--")}</>;
+    return (
+      <>
+      Test C
+
+      {this.props.users.map((element, i) => (
+         this.getTemplate(element)
+      ))}
+
+      </>
+    );
   }
 }
 
 const mapStateToProps = state => {
   return state.users;
-};
+}
 
 const mapDispatchToProps = {
-  getUsers,
-  showLoadingSpinner
-};
+getUsers,
+showLoadingSpinner
+}
 
 export default connect(
   mapStateToProps,
